1cdf49ce4ed2b676830fe3e3b679244c65f4a486
Mario de Frutos <mario.defrutos@cartodb.com>
2016-03-14 11:58:20 +0100

    Improve logging and timeouts for geocodings

--- app/models/geocoding.rb
+++ app/models/geocoding.rb
@@ -56,13 +56,13 @@ class Geocoding < Sequel::Model
     # INFO: since we want to register geocodings during importing we can't register the table
     # validates_presence :table_id
     validates_includes ALLOWED_KINDS, :kind
-  end # validate
+  end
 
   def before_save
     super
     self.updated_at = Time.now
     cancel if state == 'cancelled'
-  end # before_save
+  end
 
   def geocoding_logger
     @@geocoding_logger ||= Logger.new("#{Rails.root}/log/geocodings.log")
@@ -113,17 +113,21 @@ class Geocoding < Sequel::Model
   end
 
   def cancel
+    log.append_and_store "Cancelling job because of user request"
     table_geocoder.cancel
   rescue => e
+    log.append_and_store "Error trying to cancel a job because of user request: #{e.inspect}"
     count ||= 1
     retry unless (count = count.next) > 5
     CartoDB::notify_exception(e, user: user)
-  end # cancel
+  ensure
+    store_geocoder_info
+  end
 
   # INFO: this method shall always be called from a queue processor
   def run!
     @started_at = Time.now
-    log.append "Running geocoding job on server #{Socket.gethostname} with PID: #{Process.pid}"
+    log.append_and_store "Running geocoding job on server #{Socket.gethostname} with PID: #{Process.pid}"
     if self.force_all_rows == true
       table_geocoder.reset_cartodb_georef_status
     else
@@ -143,17 +147,18 @@ class Geocoding < Sequel::Model
   rescue => e
     # state == nil probably means it has failed even before run_geocoding begun
     handle_geocoding_failure(e, rows_geocoded_before || 0) if state == nil
-    log.append "Unexpected exception: #{e.to_s}"
-    log.append e.backtrace
+    log.append_and_store "Unexpected exception: #{e.to_s}"
+    log.append_and_store e.backtrace
     CartoDB.notify_exception(e)
     raise e
   ensure
+    store_geocoder_info
     log.store # Make sure the log is stored in DB
     user.db_service.reset_pooled_connections
   end
 
   def run_geocoding!(processable_rows, rows_geocoded_before = 0)
-    log.append "run_geocoding!()"
+    log.append_and_store "run_geocoding!()"
     self.update state: 'started', processable_rows: processable_rows
     @started_at ||= Time.now
 
@@ -171,10 +176,6 @@ class Geocoding < Sequel::Model
     handle_geocoding_success(rows_geocoded_before)
   rescue => e
     handle_geocoding_failure(e, rows_geocoded_before)
-  ensure
-    if table_geocoder && table_geocoder.remote_id
-      self.update remote_id: table_geocoder.remote_id
-    end
   end
 
   def report(error = nil)
@@ -343,7 +344,7 @@ class Geocoding < Sequel::Model
     geocoded_rows = total_geocoded_rows(rows_geocoded_before)
     self.update(state: 'finished', real_rows: geocoded_rows, used_credits: calculate_used_credits)
     send_report_mail(state, self.table_name, nil, self.processable_rows, geocoded_rows)
-    log.append "Geocoding finished"
+    log.append_and_store "Geocoding finished"
     # In the import table_service could be nil
     if !table_service.nil?
       # To store the bbox in visualizations
@@ -363,8 +364,8 @@ class Geocoding < Sequel::Model
     CartoDB::notify_exception(raised_exception, user: user)
     geocoded_rows = total_geocoded_rows(rows_geocoded_before)
     send_report_mail(state, self.table_name, error_code, self.processable_rows, geocoded_rows)
-    log.append "Unexpected exception: #{raised_exception.to_s}"
-    log.append raised_exception.backtrace
+    log.append_and_store "Unexpected exception: #{raised_exception.to_s}"
+    log.append_and_store raised_exception.backtrace
     self.report(raised_exception)
   end
 
@@ -377,6 +378,20 @@ class Geocoding < Sequel::Model
   def send_report_mail(state, table_name, error_code=nil, processable_rows, number_geocoded_rows)
     geocoding_time = @finished_at - @started_at
     CartoDB::Geocoder::MailNotifier.new(user.id, state, table_name, error_code, processable_rows, number_geocoded_rows, geocoding_time).notify_if_needed
+  end
+
+  def store_geocoder_info
+    # we use the object instead of the method because
+    # we don't want to build the object until it has
+    # all the needed data that is created in the run method
+    # of the geocoder object. If not is going to fail because
+    # the object is memoized.
+    if defined?(@table_geocoder)
+      self.geocoder_type = table_geocoder.name
+      self.batched = table_geocoder.used_batch_request?
+      self.remote_id = table_geocoder.remote_id
+      self.save
+    end
   end
 
 end
