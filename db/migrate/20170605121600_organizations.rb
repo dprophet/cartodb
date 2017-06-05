27dd57db18b92fc90ee9b0c03c302a94b876523f
Vinayak <vmanohar1@users.noreply.github.com>
2017-06-05 15:45:19 -0400

    Update 20170605121600_organizations.rb

--- db/migrate/20170605121600_organizations.rb
+++ db/migrate/20170605121600_organizations.rb
@@ -1,5 +1,5 @@
 Sequel.migration do
-  change do
+  up do
     alter_table :organizations do
       set_column_default :geocoding_block_price, 0
       set_column_default :geocoding_quota, 2000000000
@@ -8,7 +8,7 @@ Sequel.migration do
   down do
     alter_table :organizations do
       set_column_default :geocoding_block_price, nil
-      set_column_default :geocoding_quota, nil
+      set_column_default :geocoding_quota, 0
     end
   end
 end
