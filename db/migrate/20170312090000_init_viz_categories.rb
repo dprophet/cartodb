a36a475f6118b1d0808545370420787baac356c4
Mehmet Bektas <mbektas@bloomberg.net>
2017-06-05 12:21:23 -0400

    updated categories and sub-categories

--- db/migrate/20170312090000_init_viz_categories.rb
+++ db/migrate/20170312090000_init_viz_categories.rb
@@ -12,19 +12,17 @@ Sequel.migration do
       (1, 'Energy', 1, 0),
       (1, 'Vessels', 1, 0),
       (1, 'Environmental', 1, 0),
-      (1, 'Real Estate', 1, 0),
       (1, 'Banking', 1, 0),
-      (1, 'Demographics', 1, 0),
-      (1, 'Economics', 1, 0),
       (1, 'Retail', 1, 0),
       (1, 'Points of Interest', 1, 0),
       (1, 'Administrative', 1, 0),
       (1, 'Political', 1, 0),
-      (1, 'Transportation', 1, 0);"
+      (1, 'Infrastructure', 1, 0),
+      (1, 'Communications', 1, 0);"
 
     execute "INSERT INTO visualization_categories (type, name, parent_id, list_order) VALUES    
       (1, 'Exploration', 3, 0),
-      (1, 'Renewables', 3, 0),
+      (1, 'Renewable Energy', 3, 0),
       (1, 'Coal', 3, 0),
       (1, 'Natural Gas', 3, 0),
       (1, 'Oil', 3, 0),
@@ -36,14 +34,15 @@ Sequel.migration do
     execute "INSERT INTO visualization_categories (type, name, parent_id, list_order) VALUES    
       (1, 'Natural Disasters', 5, 0),
       (1, 'Climate', 5, 0),
+      (1, 'Administration', 5, 0),
       (1, 'Weather', 5, 0);"
 
     execute "INSERT INTO visualization_categories (type, name, parent_id, list_order) VALUES    
-      (1, 'Global', 12, 0),
-      (1, 'Australia', 12, 0),
-      (1, 'Asia', 12, 0),
-      (1, 'South America', 12, 0),
-      (1, 'Europe', 12, 0),
-      (1, 'North America', 12, 0);"
+      (1, 'Global', 9, 0),
+      (1, 'Australia', 9, 0),
+      (1, 'Asia', 9, 0),
+      (1, 'South America', 9, 0),
+      (1, 'Europe', 9, 0),
+      (1, 'North America', 9, 0);"
   end
 end
