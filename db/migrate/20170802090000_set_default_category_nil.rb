ab6d5ac769051be277a393ca0f7936aa97777d23
Mehmet Bektas <mbektas@bloomberg.net>
2017-10-04 14:02:18 -0400

    add default category update to db migrate script

--- db/migrate/20170802090000_set_default_category_nil.rb
+++ db/migrate/20170802090000_set_default_category_nil.rb
@@ -3,10 +3,12 @@ Sequel.migration do
     alter_table :visualizations do
       set_column_default :category, nil
     end
+    execute "UPDATE visualizations SET category=NULL WHERE category=-1;"
   end
   down do
     alter_table :visualizations do
       set_column_default :category, -1
     end
+    execute "UPDATE visualizations SET category=-1 WHERE category IS NULL;"
   end
 end
